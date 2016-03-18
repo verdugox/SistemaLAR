9d76f3786b0bb9e685df6dea50bafc0acd462117
Luis Acuna <luisandresverdugox@gmail.com>
2016-03-17 22:58:57 -0500

    Se subio css y js de plantilla para Sistema Dental

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,7 +5,11 @@ class IndexController extends ControllerBase
 
     public function indexAction()
     {
-
+        $this->assets
+                ->addCss("bower_components/bootstrap/dist/css/bootstrap.min.css");
+        $this->assets
+                ->addJs("bower_components/angular/angular.min.js")
+                ->addJs("bower_components/angular-route/angular-route.min.js");
     }
 
 }
